d2947fe02a4793d008aa8e5638f23cc1b8fa6bec
pawit.yodk <bankpawit0912@gmail.com>
2024-03-20 18:03:51 -0500

    refactor and fix bug

--- models/registeruser.py
+++ models/registeruser.py
@@ -1,18 +1,11 @@
-from wtforms import StringField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField
 from flask_wtf import FlaskForm
-
-class RegisterNewUser():
-    FirstName: str
-    LastName: str
-    EmailAddress: str
-    Password: str
-    ConfirmPass: str
 
 
 class NewUserForm(FlaskForm):
     FirstName = StringField("")
     LastName = StringField("")
     EmailAddress = StringField("")
-    Password = StringField("")
+    Password = PasswordField("")
     ConfirmPass = StringField("")
     submit = SubmitField("Submit")
